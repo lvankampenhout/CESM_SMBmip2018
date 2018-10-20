ccf31c8ab264798806d058e4a5066e5ed27aaad6
Leo van Kampenhout <l.vankampenhout@uu.nl>
2018-10-20 01:34:35 -0600

    make work for DAV nodes;

--- step_3/regrid.py
+++ step_3/regrid.py
@@ -12,10 +12,19 @@ import subprocess
 casename    = 'f.e20.FHIST.f09_001'
 #stream_tag  = 'h2' # stream identifier for vector data (h0, h1, or h2)
 outdir      = '/glade/u/home/lvank/scratch/archive/smbmip/' # post-fix /$CASENAME/dstGrid3d/varname will be added
-scrip_dst   = '/gpfs/u/home/lvank/github/libvector/smbmip/step_2/dst_SCRIP.nc' # SCRIP file of destination grid (from step 2)
+scrip_dst   = '/glade/u/home/lvank/projects/SMBmip//step_2/dst_SCRIP.nc' # SCRIP file of destination grid (from step 2)
 remap_wgt    = 'remap_weights.nc' # temporary file
 
-varlist = 'RAIN SNOW QICE QRUNOFF QSNOMELT'.split()
+varlist = []
+varlist += 'RAIN SNOW QICE QRUNOFF QSNOMELT'.split()
+varlist += 'QICE_MELT QSOIL'.split()
+varlist += 'FSA FSR'.split()
+varlist += 'FIRE FIRA'.split()
+varlist += 'FSH EFLX_LH_TOT TSA'.split()
+
+CDO_CMD='/glade/u/apps/dav/opt/cdo/1.9.3/gnu/7.3.0/bin/cdo' # dav
+#CDO_CMD='cdo'
+
 
 for varname in varlist:
 
@@ -35,16 +44,21 @@ for varname in varlist:
 
       if (not os.path.exists(remap_wgt)):
          # do once per destination grid
-         args = ['cdo']
+         args = [CDO_CMD]
          args += ['genbil,%s' % scrip_dst, infile, remap_wgt]
          subprocess.check_call(args, stderr=subprocess.STDOUT)
 
       basename = infile.split('/')[-1] # filename without path
       outfile = os.path.join(outdir_var, basename)
 
-      args = ['cdo']
+      if (os.path.exists(outfile)): 
+         print("INFO: file exists, skipping: "+outfile)
+         continue
+
+      args = [CDO_CMD]
       args += ['--format', 'nc4', '-b', 'F32']
       #args += ['-z', 'zip1']
       args += ['remap,%s,%s' % (scrip_dst, remap_wgt), infile, outfile]
       print(infile)
+      print(args)
       subprocess.check_call(args, stderr=subprocess.STDOUT)
